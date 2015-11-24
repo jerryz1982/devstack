d2e626934293e6c1619aa1b96e41e98798589684
Jerry Zhao <xyzjerry@gmail.com>
2015-11-24 20:07:54 +0000

    add demo scripts

---
+++ demo_wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ ! $1 ]]; then
+    echo "usage: ./demo_wrapper.sh tenant_name"
+    exit 1
+fi
+./demo.sh add-tenant $1 ||exit $?
+./demo.sh create-public-net $1 ||exit $?
+./demo.sh create-tenant-net $1 ||exit $?
+./demo.sh boot-vm $1 ||exit $?
+./demo.sh associate-floatingip $1 ||exit $?
